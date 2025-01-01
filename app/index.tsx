b33cdcc41a40217cbf26707184969dcc4a528536
Rowoon <dev.rowoon@gmail.com>
2025-01-01 16:16:08 +0900

    study: 폴더 변경 및 리액트 복습2

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native";
 import { GestureHandlerRootView, ScrollView, TextInput } from 'react-native-gesture-handler';
-import Cat from './Cat';
+import Cat from './study/Cat';
+import Pizza from './study/Pizza';
 
 console.log(Cat);
 
@@ -9,6 +10,7 @@ export default function Index() {
     <GestureHandlerRootView style={{ flex: 1}}>
       <ScrollView>   
         <Cat name="mango"/>
+        <Pizza />
       </ScrollView>
     </GestureHandlerRootView>
 
