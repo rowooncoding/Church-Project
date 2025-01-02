6409c635c08accc403d598708f26e61a3e080ca2
Rowoon <dev.rowoon@gmail.com>
2025-01-02 12:18:13 +0900

    study: 리액트 네이티브 플랫 리스트 정적 리스트 학습

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,14 @@ import { Text, View } from "react-native";
 import { GestureHandlerRootView, ScrollView, TextInput } from 'react-native-gesture-handler';
 import Cat from './study/Cat';
 import Pizza from './study/Pizza';
+import FlatListBasics from './study/FlatListBasics';
 
 console.log(Cat);
 
 export default function Index() {
   return (
     <GestureHandlerRootView style={{ flex: 1}}>
+      <FlatListBasics />
       <ScrollView>   
         <Cat name="mango"/>
         <Pizza />
